96f181da530f9fd646a8a54a305b5e8570de006e
Ashutosh Kr Pandey <92009208+AshutoshPandey05@users.noreply.github.com>
2021-10-18 15:17:34 +0530

    Add files via upload

---
+++ MyClass1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Destructor_Finalize
+{
+   public class MyClass1
+    {
+        public void Display_Msg()
+        {
+            Console.WriteLine("Inside Display Method()");
+        }
+       public MyClass1 ()
+        {
+            Console.WriteLine("Default Constructor..");
+        }
+       public MyClass1 ( string msg)
+        {
+            Console.WriteLine("Parametrized Constructor with a Message {0}", msg);
+        }
+        ~MyClass1()
+        {
+            Console.WriteLine("Freeing up Resources...");
+        }
+        public void Dispose()
+        {
+            Console.WriteLine("Freeing Up Resouces using Dispose");
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Demo_Destructor_Finalize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Demo Finilizer..");
+            MyClass1 obj1 = new MyClass1();
+            obj1.Display_Msg();
+
+            //MyClass1 obj2 = new MyClass1("Secret Message");
+            //obj2.Display_Msg();
+
+            obj1.Dispose();
+        }
+    }
+}
